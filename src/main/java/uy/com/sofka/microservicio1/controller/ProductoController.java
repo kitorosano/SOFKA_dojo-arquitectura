efe750b9ce7a5c87c3ec60c581f6205b7b0d041a
Kitorosano <rosanoesteban2@gmail.com>
2022-04-12 18:59:37 -0300

    fixing endpoint mapping

--- src/main/java/uy/com/sofka/microservicio1/controller/ProductoController.java
+++ src/main/java/uy/com/sofka/microservicio1/controller/ProductoController.java
@@ -3,26 +3,22 @@ package uy.com.sofka.microservicio1.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import uy.com.sofka.microservicio1.model.Producto;
 import uy.com.sofka.microservicio1.service.IProductoService;
 
 @RestController
-@RequestMapping("/productos")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProductoController {
   
   @Autowired
   private IProductoService service;
 
-  @GetMapping("")
-  List<Producto> listar() {
+  @GetMapping("/")
+  public List<Producto> listar() {
     return service.listar();
   }
 }
